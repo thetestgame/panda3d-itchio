98c2567e77278c7eb34402dad33a4cd74c0a9bf6
thetestgame <nxtlabs13@gmail.com>
2024-10-26 19:39:34 -0500

    Misc. adjustments

--- setup.py
+++ setup.py
@@ -1,25 +1,5 @@
 """
-MIT License
-
-Copyright (c) 2024 Jordan Maxwell
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+Setup script for building the module into a package for publishing to PyPI.
 """
 
 from setuptools import setup
@@ -42,8 +22,8 @@ def get_readme(filename: str = 'README.md') -> str:
     Returns the contents of the requested README file
     """
 
-    with open(filename, 'r') as f:
-        return f.read()
+    with open(filename, 'r') as file:
+        return file.read()
 
 def get_requirements(filename: str = 'requirements.txt') -> list:
     """
